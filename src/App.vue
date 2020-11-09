175b4586ef4de55b8356e0e5694ee292bf472290
Evenming <a15307607262010@qq.com>
2020-11-09 23:14:47 +0800

    登录模块-路由整合

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
-  <div>
-    <button>点我有惊喜</button>
-    <router-link to="">测试</router-link>
-    <router-view></router-view>
-     <el-carousel height="150px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3 class="small">{{ item }}</h3>
-      </el-carousel-item>
-    </el-carousel>
+  <div class="app-container">
+      <router-view></router-view>
   </div>
 </template>
 
@@ -18,10 +11,5 @@ export default {
 </script>
 
 <style lang="less">
-  div {
-
-    button {
-      background-color: cyan;
-    }
-  }
+  
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 
 import VueRouter from 'vue-router'
 
+import login from '@/views/login'
+
 Vue.use(VueRouter)
 
 
-const routes = []
+const routes = [
+  {path:'/',redirect:'/login'},
+  {path:'/login',component:login}
+]
 
 const router = new VueRouter({
   routes
