64f9a219c2aceb91812219aa45c4fd469477e8a7
Evenming <a15307607262010@qq.com>
2020-11-09 21:07:52 +0800

    Element-ui整合

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <button>点我有惊喜</button>
     <router-link to="">测试</router-link>
     <router-view></router-view>
+     <el-carousel height="150px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
